bce1bec47a65b86b7f654c58dcb743411cb5ee1d
Robert Palmer <robert.palmer@artcom.de>
2010-02-04 13:18:11 +0100

    added cast to remove a compiler warning

--- SCPlotCanvas.m
+++ SCPlotCanvas.m
@@ -76,7 +76,7 @@
 		return NSMakeRect(0, -1, width, 2);
 	}
 	
-	float currentMaxWidth = [[graphs objectAtIndex:0] width];
+	float currentMaxWidth = [(SCGraph *)[graphs objectAtIndex:0] width];
 	
 	float startingValue = currentMaxWidth - width;
 	if (startingValue < 0) {
